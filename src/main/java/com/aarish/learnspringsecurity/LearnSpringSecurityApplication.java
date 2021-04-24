bccee19fe79310bcbc6c598a518fe934a53da4a2
Aarish S <aarish.work@gmail.com>
2021-04-24 09:00:21 +0530

    in memory authentication

---
+++ src/main/java/com/aarish/learnspringsecurity/LearnSpringSecurityApplication.java
@@ -0,0 +1,67 @@
+package com.aarish.learnspringsecurity;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+public class LearnSpringSecurityApplication {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager();
+    }
+
+    @Bean
+    InitializingBean initializer(UserDetailsManager manager){
+        return ()->{
+
+            UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
+            manager.createUser(admin);
+
+            UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build();
+            manager.createUser(user);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringSecurityApplication.class, args);
+    }
+
+}
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
+
+@RestController
+class HelloController{
+
+    @GetMapping("/greetings")
+    String greeting(Principal principal){
+        return "Hello " + principal.getName() + "!!";
+    }
+}
---
+++ src/test/java/com/aarish/learnspringsecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarish.learnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
